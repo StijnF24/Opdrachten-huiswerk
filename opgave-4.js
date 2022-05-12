4557be81ae56aa1a32ce4b91d58e48fe7530cbdb
StijnF24 <112707@emmauscollege.nl>
2022-05-12 10:08:40 +0000

    opgave 1 af

--- opgave-4.js
+++ opgave-4.js
@@ -6,14 +6,14 @@ module.exports = {alleCijfers}
    cijfers 0 t/m 9 print
 */
 
-function alleCijfers() {
-  var uitkomst = "";
+function alleCijfers(teller) {
+  var uitkomst = "0123456789";
     
   // maak voorwaarde van de for-loop juist en schrijf de juiste regel IN de for-loop
-  for (var teller = 0; teller < ___ ; teller++) {
+  for (var teller = 0; teller < 10 ; teller++) {
 
       //typ hieronder nog één regel code om de opdracht op te lossen 
-      
+uitkomst= "0123456789"
   }
   
   return uitkomst;
